0aa1a01fe868f161f808fc30ccb6fa85cd8f949f
MariaD04 <117981238+MariaD04@users.noreply.github.com>
2024-05-17 02:14:52 +0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import React, { Component } from 'react' 
 import Note from './components/Note'
 import Form from './components/Form'
 import './App.css'
-
-import React, { Component } from 'react' 
  
 export default class App extends Component { 
- 
   constructor(props) { 
     super(props) 
     this.state = { 
